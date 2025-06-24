f0626b860dbc1d029e8ec1b2b95d07425fcd99a7
DoukheeWon <fain9301@yahoo.com>
2025-06-24 12:50:07 +0900

    fix: text springboottest annotation remove unit tests

--- src/test/java/kr/co/won/inflearnspringaistudy/basic_flatmap/FlatMapOperatorTests.java
+++ src/test/java/kr/co/won/inflearnspringaistudy/basic_flatmap/FlatMapOperatorTests.java
@@ -6,7 +6,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
-@SpringBootTest
+//@SpringBootTest
 public class FlatMapOperatorTests {
 
     /**
